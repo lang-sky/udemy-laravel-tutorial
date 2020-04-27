6db5ed888243f595cf495ddbea63f5eb400a56cc
lang-sky <skylang121@outlook.com>
2020-04-28 01:08:25 +0800

    9. creating the database structure using Migrations

--- database/migrations/2020_04_27_155306_create_transactions_table.php
+++ database/migrations/2020_04_27_155306_create_transactions_table.php
@@ -14,8 +14,14 @@ class CreateTransactionsTable extends Migration
     public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('quantity')->unsigned();
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foregin('product_id')->references('id')->on('products');
         });
     }
 
