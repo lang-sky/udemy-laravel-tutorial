76b77ac1a37dfef167f51751b0aa636c90b6f754
lang-sky <skylang121@outlook.com>
2020-04-28 00:11:25 +0800

    7. creating the initial laravel components for RESTful API

---
+++ database/factories/BuyerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Buyer;
+use Faker\Generator as Faker;
+
+$factory->define(Buyer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
