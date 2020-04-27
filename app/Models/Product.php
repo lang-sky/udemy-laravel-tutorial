79da1587b066437793c2d0acff413978abff1559
lang-sky <skylang121@outlook.com>
2020-04-28 00:50:51 +0800

    8. models and relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = 'availalbe';
+    const UNAVAILABLE_PRODUCT = 'unavailalbe';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailalbe ()
+    {
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    public function seller ()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions ()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories ()
+    {
+        return $this->belongToMany(Category::class);
+    }
 }
--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -7,5 +7,19 @@ use App\User;
 
 class Seller extends User
 {
-    //
+    protected $fillable = [
+        'quantity',
+        'buyer_id',
+        'product_id',
+    ];
+
+    public function buyer ()
+    {
+        return $this->belongsTo(Buyer::class);
+    }
+
+    public function product ()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
